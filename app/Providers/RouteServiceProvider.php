c7b7e5114ce7c28eeb6fa2785821e9712c39b79b
Bharath <bharath.bommidi999@gmail.com>
2024-12-23 11:46:52 +0530

    issues fixed

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -34,10 +34,10 @@ class RouteServiceProvider extends ServiceProvider
                 ->prefix('api')
                 ->group(base_path('routes/api.php'));
 
-            Route::middleware('web')->group(base_path('routes/web.php'));
-
             Route::middleware(['web', 'auth', 'role:admin'])
                 ->group(base_path('routes/admin.php'));
+
+            Route::middleware('web')->group(base_path('routes/web.php'));
         });
     }
 }
